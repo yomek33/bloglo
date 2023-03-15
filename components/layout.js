8b97951bb17b9a5861c385f4e322a47c4db5e832
yomek33 <i3landme@gmail.com>
2023-03-16 02:25:05 +0900

    fix

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Footer from "components/footer";
 export default function Layout({ children }) {
   return (
     <>
-      <div class="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen">
         <Header />
 
         <main>{children}</main>
